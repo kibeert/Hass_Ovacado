69f01e3de28a1228b1fca297f8b2a8b05f6e1798
Collinsemmanuel <emmanuelkibet42018@gmail.com>
2023-07-19 21:30:39 +0300

    Hass Ovacado

---
+++ add.js
@@ -0,0 +1,15 @@
+function largest(){
+    let a, b, c;
+a= parseInt(prompt("Enter the value of a","a"));
+b= parseInt(prompt("Enter the value of b","b"));
+c= parseInt(prompt("Enter the value of c","c"));
+
+if(a>b&&a>c){
+    alert("a is greatest ")
+}
+else if(b>a$$b>c){
+    alert("b is greatest")
+}
+else{
+    alert("c is the greatest")
+}}
